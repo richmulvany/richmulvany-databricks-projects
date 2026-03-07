f39aa5653c8d187b315c1f47a45984c9b6a13209
Richard Mulvany <143214470+richmulvany@users.noreply.github.com>
2026-03-07 19:39:49 +0000

    Change engine path

--- hr-chatbot/backend/app/db.py
+++ hr-chatbot/backend/app/db.py
@@ -6,6 +6,8 @@ def get_database() -> SQLDatabase:
     host = os.getenv("DATABRICKS_HOST")
     token = os.getenv("DATABRICKS_TOKEN")
     http_path = os.getenv("DATABRICKS_WAREHOUSE_ID")
+    catalog = os.getenv("DATABRICKS_CATALOG")
+    schema = os.getenv("DATABRICKS_SCHEMA")
 
     if not host or not token or not http_path:
         raise ValueError("DATABRICKS_HOST, DATABRICKS_TOKEN, and DATABRICKS_WAREHOUSE_ID must be set")
@@ -13,5 +15,10 @@ def get_database() -> SQLDatabase:
     engine = create_engine(
         f"databricks+connector://token:{token}@{host}:443/{http_path}&catalog=03_gold&schema=hr"
     )
+
+    engine = create_engine(
+  url = f"databricks://token:{token}@{host}?" +
+        f"http_path={http_path}&catalog={catalog}&schema={schema}"
+)
 
     return SQLDatabase(engine)
